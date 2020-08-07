e06eef2df66b9f21e39be0f4ed63f33c23b3b8bb
Roberto Rodriguez <roberto@rodriguezartavia.com>
2020-08-07 14:54:56 -0600

    push

--- views/Layout.js
+++ views/Layout.js
@@ -8,12 +8,29 @@ export default function MyApp(props) {
   return (
     <React.Fragment>
       <Head>
-        <title>rodriguezLab.co</title>
+        <title>Laboratorio de Emprendedurismo Social y Tecnológico</title>
 
         <link rel="icon" href="/favicon.ico" />
         <meta
+          name="description"
+          content="El centro de desarrollo de prototipos del inventor Roberto Rodríguez para la solución de los problemas humanos a través de innovaciones organizacionales y tecnológicas."
+        />
+
+        <meta
           name="viewport"
           content="minimum-scale=1, initial-scale=1, width=device-width"
+        />
+
+        <meta property="og:site_name" content="RodriguezLab" key="ogsitename" />
+        <meta
+          property="og:title"
+          content="Laboratorio de Emprendedurismo Social y Tecnológico"
+          key="ogtitle"
+        />
+        <meta
+          property="og:description"
+          content="El centro de desarrollo de prototipos del inventor Roberto Rodríguez para la solución de los problemas humanos a través de innovaciones organizacionales y tecnológicas."
+          key="ogdesc"
         />
       </Head>
 
