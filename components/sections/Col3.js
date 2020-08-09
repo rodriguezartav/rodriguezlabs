0a3108bb04929c01db78aba30dee24616679831f
Roberto Rodriguez <roberto@rodriguezartavia.com>
2020-08-08 19:32:33 -0600

    push

--- components/sections/Col3.js
+++ components/sections/Col3.js
@@ -17,29 +17,38 @@ export default function col3(props) {
           {props.bullets.map((item) => {
             return (
               <div className="mb-10 md:mb-0 lg:mb-0">
-                <div className="flex items-center justify-center h-12 w-12 rounded-md bg-red-500 text-white">
-                  <svg
-                    className="h-6 w-6"
-                    fill="none"
-                    viewBox="0 0 24 24"
-                    stroke="currentColor"
-                  >
-                    <path
-                      stroke-linecap="round"
-                      stroke-linejoin="round"
-                      stroke-width="2"
-                      d="M21 12a9 9 0 01-9 9m9-9a9 9 0 00-9-9m9 9H3m9 9a9 9 0 01-9-9m9 9c1.657 0 3-4.03 3-9s-1.343-9-3-9m0 18c-1.657 0-3-4.03-3-9s1.343-9 3-9m-9 9a9 9 0 019-9"
-                    />
-                  </svg>
-                </div>
+                {!item.icon && (
+                  <div className="flex items-center justify-center h-12 w-12 rounded-md bg-red-500 text-white">
+                    <svg
+                      className="h-6 w-6"
+                      fill="none"
+                      viewBox="0 0 24 24"
+                      stroke="currentColor"
+                    >
+                      <path
+                        stroke-linecap="round"
+                        stroke-linejoin="round"
+                        stroke-width="2"
+                        d="M21 12a9 9 0 01-9 9m9-9a9 9 0 00-9-9m9 9H3m9 9a9 9 0 01-9-9m9 9c1.657 0 3-4.03 3-9s-1.343-9-3-9m0 18c-1.657 0-3-4.03-3-9s1.343-9 3-9m-9 9a9 9 0 019-9"
+                      />
+                    </svg>
+                  </div>
+                )}
+
                 <div className="mt-5">
                   <h5 className="text-lg leading-6 font-medium text-gray-900">
                     {item.title}
                   </h5>
-                  <p className="mt-2 text-base leading-6 text-gray-500">
+                  <p className="mt-2 h-40 text-base leading-6 text-gray-500">
                     {item.text}
                   </p>
                 </div>
+
+                {item.icon && (
+                  <div className="mt-3">
+                    <img src={item.icon.url} />
+                  </div>
+                )}
               </div>
             );
           })}
